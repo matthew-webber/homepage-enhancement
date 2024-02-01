d18d02f4012cc9590d59900c2829a6a029c2ddc9
Matt Webber <dev.matthew.lee.webber@gmail.com>
2024-02-01 17:28:19 -0500

    Edits and adds alt text, fixes services image bug (too tall would overflow, too small would not hold proportions)

--- src/templates/services.js
+++ src/templates/services.js
@@ -4,182 +4,183 @@ services = html`
   class="tw-leading-6 tw-font-nunito-sans tw-w-full lg:tw-flex lg:tw-justify-center"
 >
   <div
-    class="tw-max-w-[1000px] tw-py-10 tw-px-5 md:tw-p-10 lg:tw-py-20 lg:tw-px-0 lg:tw-mx-12"
+    class="tw-max-w-[1000px] tw-w-full tw-bg-white tw-py-10 tw-px-5 md:tw-p-10 lg:tw-py-20 lg:tw-px-0 lg:tw-mx-12"
   >
-    <div class="tw-w-full tw-bg-white tw-flex tw-flex-col">
-      <h2
-        class="tw-mb-10 tw-text-[26px]/[28px] md:tw-text-4xl tw-font-bold tw-text-musc-blue md:tw-mb-8"
-      >
-        Best-in-Class Services
-      </h2>
-      <div class="tw-flex tw-w-full md:tw-gap-10 lg:tw-gap-12">
-        <!-- Services -->
-        <div class="tw-flex tw-flex-col tw-w-full md:tw-w-1/2">
-          <a
-            href="https://hollingscancercenter.musc.edu/patient-care"
-            class="hover-service tw-flex tw-justify-between tw-items-center tw-pb-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
-          >
-            <!-- Service Info -->
-            <div
-              class="hover-highlight-bar tw-flex tw-flex-col tw-text-musc-blue"
-            >
-              <h3
-                class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
-              >
-                Cancer Care
-              </h3>
-              <p class="tw-text-musc-blue">
-                Hollings ranked high performing hospital
-              </p>
-              <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
-            </div>
-            <!-- Arrow -->
-            <div class="hover-arrow-tail tw-pl-5">
-              <svg
-                width="15"
-                height="26"
-                viewBox="0 0 15 26"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="tw-block tw-align-middle"
-              >
-                <path
-                  d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
-                  fill="#00447C"
-                />
-              </svg>
-            </div>
-          </a>
-          <a
-            href="https://muschealth.org/medical-services/womens"
-            class="hover-service tw-flex tw-justify-between tw-items-center tw-py-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
-          >
-            <!-- Service Info -->
-            <div class="hover-highlight-bar tw-flex tw-flex-col">
-              <h3
-                class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
-              >
-                Women&rsquo;s Health
-              </h3>
-              <p class="tw-text-musc-blue">
-                #19 in the nation for obstetrics & gynecology
-              </p>
-              <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
-            </div>
-            <!-- Arrow -->
-            <div class="hover-arrow-tail tw-pl-5">
-              <svg
-                width="15"
-                height="26"
-                viewBox="0 0 15 26"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="tw-block tw-align-middle tw-transition-all tw-duration-150"
-              >
-                <path
-                  d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
-                  fill="#00447C"
-                />
-              </svg>
-            </div>
-          </a>
-          <a
-            href="https://muschealth.org/medical-services/ent"
-            class="hover-service tw-flex tw-justify-between tw-items-center tw-py-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
-          >
-            <!-- Service Info -->
-            <div class="hover-highlight-bar tw-flex tw-flex-col">
-              <h3
-                class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
-              >
-                Ear, Nose & Throat
-              </h3>
-              <p class="tw-text-musc-blue">#23 in the nation</p>
-              <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
-            </div>
-            <!-- Arrow -->
-            <div class="hover-arrow-tail tw-pl-5">
-              <svg
-                width="15"
-                height="26"
-                viewBox="0 0 15 26"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="tw-block tw-align-middle"
-              >
-                <path
-                  d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
-                  fill="#00447C"
-                />
-              </svg>
-            </div>
-          </a>
-          <a
-            href="https://musckids.org"
-            class="hover-service tw-flex tw-justify-between tw-items-center tw-py-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
-          >
-            <!-- Service Info -->
-            <div class="hover-highlight-bar tw-flex tw-flex-col">
-              <h3
-                class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
-              >
-                Children&rsquo;s Health
-              </h3>
-              <p class="tw-text-musc-blue">#9 in the southeast Region</p>
-              <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
-            </div>
-            <!-- Arrow -->
-            <div class="hover-arrow-tail tw-pl-5">
-              <svg
-                width="15"
-                height="26"
-                viewBox="0 0 15 26"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="tw-block tw-align-middle"
-              >
-                <path
-                  d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
-                  fill="#00447C"
-                />
-              </svg>
-            </div>
-          </a>
-        </div>
-        <!-- Image -->
-        <div class="tw-w-0 md:tw-w-1/2">
-          <img
-            class="tw-object-cover tw-w-full md:tw-h-full md:tw-object-cover md:tw-flex lg:tw-object-cover lg:tw-justify-self-center lg:tw-w-auto lg:tw-h-full"
-            src="/-/media/5C54627F7F0F401CAEC846A99A10A3A0.ashx"
-            alt="MUSC Health COVID-19 Vaccine Information"
-          />
-        </div>
-      </div>
-      <button
-        class="tw-flex tw-font-arial tw-text-base tw-mt-10 tw-w-full tw-cursor-auto md:tw-w-auto lg:tw-mt-12"
-      >
+    <h2
+      class="tw-mb-10 tw-text-[26px]/[28px] md:tw-text-4xl tw-font-bold tw-text-musc-blue md:tw-mb-8"
+    >
+      Best-in-Class Services
+    </h2>
+    <div class="tw-flex tw-w-full md:tw-gap-10 lg:tw-gap-12">
+      <!-- Services -->
+      <div class="tw-flex tw-flex-col tw-w-full md:tw-w-1/2">
         <a
-          href="https://muschealth.org/medical-services"
-          class="hover-arrow tw-flex tw-border-b-0 tw-justify-center tw-overflow-hidden tw-grow tw-py-3 tw-px-5 tw-h-full tw-font-bold tw-text-white tw-bg-musc-blue tw-rounded-lg md:tw-grow-0"
+          href="https://hollingscancercenter.musc.edu/patient-care"
+          class="hover-service tw-flex tw-justify-between tw-items-center tw-pb-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
         >
-          View All Services
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2.0"
-            stroke="currentColor"
-            class="tw-block tw-align-middle tw-w-0 h-6 tw-duration-150"
+          <!-- Service Info -->
+          <div
+            class="hover-highlight-bar tw-flex tw-flex-col tw-text-musc-blue"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H9"
-            />
-          </svg>
+            <h3
+              class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
+            >
+              Cancer Care
+            </h3>
+            <p class="tw-text-musc-blue">
+              Hollings ranked high performing hospital
+            </p>
+            <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
+          </div>
+          <!-- Arrow -->
+          <div class="hover-arrow-tail tw-pl-5">
+            <svg
+              alt=""
+              width="15"
+              height="26"
+              viewBox="0 0 15 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="tw-block tw-align-middle"
+            >
+              <path
+                d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
+                fill="#00447C"
+              />
+            </svg>
+          </div>
         </a>
-      </button>
+        <a
+          href="https://muschealth.org/medical-services/womens"
+          class="hover-service tw-flex tw-justify-between tw-items-center tw-py-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
+        >
+          <!-- Service Info -->
+          <div class="hover-highlight-bar tw-flex tw-flex-col">
+            <h3
+              class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
+            >
+              Women&rsquo;s Health
+            </h3>
+            <p class="tw-text-musc-blue">
+              #19 in the nation for obstetrics & gynecology
+            </p>
+            <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
+          </div>
+          <!-- Arrow -->
+          <div class="hover-arrow-tail tw-pl-5">
+            <svg
+              alt=""
+              width="15"
+              height="26"
+              viewBox="0 0 15 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="tw-block tw-align-middle tw-transition-all tw-duration-150"
+            >
+              <path
+                d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
+                fill="#00447C"
+              />
+            </svg>
+          </div>
+        </a>
+        <a
+          href="https://muschealth.org/medical-services/ent"
+          class="hover-service tw-flex tw-justify-between tw-items-center tw-py-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
+        >
+          <!-- Service Info -->
+          <div class="hover-highlight-bar tw-flex tw-flex-col">
+            <h3
+              class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
+            >
+              Ear, Nose & Throat
+            </h3>
+            <p class="tw-text-musc-blue">#23 in the nation</p>
+            <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
+          </div>
+          <!-- Arrow -->
+          <div class="hover-arrow-tail tw-pl-5">
+            <svg
+              alt=""
+              width="15"
+              height="26"
+              viewBox="0 0 15 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="tw-block tw-align-middle"
+            >
+              <path
+                d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
+                fill="#00447C"
+              />
+            </svg>
+          </div>
+        </a>
+        <a
+          href="https://musckids.org"
+          class="hover-service tw-flex tw-justify-between tw-items-center tw-py-6 tw-border-b tw-border-[#699FBA] tw-border-solid"
+        >
+          <!-- Service Info -->
+          <div class="hover-highlight-bar tw-flex tw-flex-col">
+            <h3
+              class="tw-text-lg tw-font-black tw-text-musc-blue lg:tw-font-extrabold lg:tw-text-2xl"
+            >
+              Children&rsquo;s Health
+            </h3>
+            <p class="tw-text-musc-blue">#9 in the Southeast Region</p>
+            <p class="tw-text-musc-blue tw-italic">US News & World Report</p>
+          </div>
+          <!-- Arrow -->
+          <div class="hover-arrow-tail tw-pl-5">
+            <svg
+              alt=""
+              width="15"
+              height="26"
+              viewBox="0 0 15 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              class="tw-block tw-align-middle"
+            >
+              <path
+                d="M15 13L0.937501 25.9904L0.937502 0.00961814L15 13Z"
+                fill="#00447C"
+              />
+            </svg>
+          </div>
+        </a>
+      </div>
+      <!-- Image -->
+      <img
+        class="tw-aspect-square tw-w-0 md:tw-object-cover md:tw-w-1/2"
+        src="/-/media/5C54627F7F0F401CAEC846A99A10A3A0.ashx"
+        alt=""
+      />
     </div>
+    <button
+      class="tw-flex tw-font-arial tw-text-base tw-mt-10 tw-w-full tw-cursor-auto md:tw-w-auto lg:tw-mt-12"
+    >
+      <a
+        href="https://muschealth.org/medical-services"
+        class="hover-arrow tw-flex tw-border-b-0 tw-justify-center tw-overflow-hidden tw-grow tw-py-3 tw-px-5 tw-h-full tw-font-bold tw-text-white tw-bg-musc-blue tw-rounded-lg md:tw-grow-0"
+      >
+        View All Services
+        <svg
+          alt=""
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2.0"
+          stroke="currentColor"
+          class="tw-block tw-align-middle tw-w-0 h-6 tw-duration-150"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H9"
+          />
+        </svg>
+      </a>
+    </button>
   </div>
 </div>
 `;
